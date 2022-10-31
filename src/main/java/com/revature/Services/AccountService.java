a1d86f2efbb895d51c7391dbb8b0cecb4787dabb
IDD00 <67290544+IDD00@users.noreply.github.com>
2022-10-31 15:05:04 -0400

    Updated services for Account and Transaction

--- src/main/java/com/revature/Services/AccountService.java
+++ src/main/java/com/revature/Services/AccountService.java
@@ -1,12 +1,44 @@
 package com.revature.Services;
 
 import com.revature.Controllers.AccountController;
+import com.revature.Models.Account;
+import com.revature.Models.Transaction;
 import com.revature.Repos.AccountRepo;
+
+import java.util.List;
 
 public class AccountService {
     AccountRepo accountRepo;
 
-    public AccountService(){
+    public AccountService() {
         this.accountRepo = new AccountRepo();
+    }
+
+    public AccountService(AccountRepo accountRepo) {
+        this.accountRepo = accountRepo;
+    }
+
+    public int createAccount(Account account) {
+        return accountRepo.create(account);
+    }
+
+    public List<Account> getAllAccounts(int userId) {
+        return accountRepo.getAll(userId);
+    }
+
+    public Account getAccountById(int id) {
+        return accountRepo.getById(id);
+    }
+
+    public Account updateAccount(Account account) {
+        return accountRepo.update(account);
+    }
+
+    public Account trackIncome(Account account, Transaction income) {
+        return accountRepo.trackIncome(account, income);
+    }
+
+    public Account trackExpenses(Account account, Transaction expenses) {
+        return accountRepo.trackExpenses(account, expenses);
     }
 }
--- src/main/java/com/revature/Services/TransService.java
+++ src/main/java/com/revature/Services/TransService.java
@@ -3,14 +3,24 @@ package com.revature.Services;
 import com.revature.Models.Transaction;
 import com.revature.Repos.TransRepo;
 
+import java.util.List;
+
 public class TransService {
     TransRepo transRepo;
 
-    public TransService(){
+    public TransService() {
         this.transRepo = new TransRepo();
+    }
+
+    public TransService(TransRepo transRepo) {
+        this.transRepo = transRepo;
     }
 
     public Transaction createTransaction(Transaction transaction) {
         return transRepo.create(transaction);
+    }
+
+    public List<Transaction> getAllTransactions(int accountId) {
+        return transRepo.getAll(accountId);
     }
 }
