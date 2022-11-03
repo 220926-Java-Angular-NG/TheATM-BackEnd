da61e739782cee676cc0d78e74b95de4ec428432
Matt Hanson <matthanwork@gmail.com>
2022-11-03 15:27:19 -0400

    Moved old database into archive. added spring boot

---
+++ src/main/java/com/revashare/theatmbackend/TheAtmBackendApplication.java
@@ -0,0 +1,13 @@
+package com.revashare.theatmbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheAtmBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheAtmBackendApplication.class, args);
+	}
+
+}
