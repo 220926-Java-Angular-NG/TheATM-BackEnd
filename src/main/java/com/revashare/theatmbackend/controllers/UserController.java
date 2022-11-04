6fa0e26f13c447756b67c0d0b02b8af3525f1666
MaHa-Work <114355529+MaHa-Work@users.noreply.github.com>
2022-11-03 22:10:12 -0400

    added create user

--- src/main/java/com/revashare/theatmbackend/controllers/UserController.java
+++ src/main/java/com/revashare/theatmbackend/controllers/UserController.java
@@ -1,7 +1,10 @@
 package com.revashare.theatmbackend.controllers;
 
+import com.revashare.theatmbackend.models.User;
 import com.revashare.theatmbackend.services.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,4 +13,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class UserController {
     private final UserService userService;
+
+    @PostMapping
+    public User createUser(@RequestBody User user){
+        return userService.createUser(user);
+    }
 }
