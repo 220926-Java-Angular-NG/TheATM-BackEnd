4240d35bb8aeb3108878effd03c7dddebced0ac1
MaHa-Work <114355529+MaHa-Work@users.noreply.github.com>
2022-11-06 21:23:16 -0500

    Added shopping mall logic as onstartup class.

--- src/main/java/com/revashare/theatmbackend/TheAtmBackendApplication.java
+++ src/main/java/com/revashare/theatmbackend/TheAtmBackendApplication.java
@@ -1,8 +1,12 @@
 package com.revashare.theatmbackend;
 
+
 import com.revashare.theatmbackend.models.Account;
 import com.revashare.theatmbackend.models.Transaction;
 import com.revashare.theatmbackend.models.User;
+import com.revashare.theatmbackend.services.AccountService;
+import com.revashare.theatmbackend.services.UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
@@ -10,13 +14,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class})
 public class TheAtmBackendApplication {
 	@Autowired
 	JdbcTemplate jdbcTemplate;
+
 	public static void main(String[] args) {
 		SpringApplication.run(TheAtmBackendApplication.class, args);
 	}
